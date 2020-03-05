2c3cbb87a377225b0b72c0b889fdf279b16d9846
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2020-03-05 16:04:36 +0100

    no service context required at processor side

--- core/src/main/java/org/fao/geonet/kernel/datamanager/base/BaseMetadataIndexer.java
+++ core/src/main/java/org/fao/geonet/kernel/datamanager/base/BaseMetadataIndexer.java
@@ -377,7 +377,7 @@ public class BaseMetadataIndexer implements IMetadataIndexer, ApplicationEventPu
                         moreFields.add(
                             SearchManager.makeField(Geonet.IndexFieldNames.XLINK, xlink.getValue(), true, true));
                     }
-                    Processor.detachXLink(md, getServiceContext());
+                    Processor.detachXLink(md);
                 } else {
                     moreFields.add(SearchManager.makeField(Geonet.IndexFieldNames.HASXLINKS, "0", true, true));
                 }
